2efd845507ebf61e46a1c44accb032439edffe3e
unknown <lfgalang@unal.edu.co>
2023-07-05 10:15:16 -0500

    allowed the policies

--- js/router.js
+++ js/router.js
@@ -1,3 +1,4 @@
+// Definición de la función de enrutamiento
 const route = (event) => {
     event = event || window.event;
     event.preventDefault();
@@ -5,6 +6,7 @@ const route = (event) => {
     handleLocation();
 };
 
+// Definición de las rutas disponibles
 const routes = {
     404: "/pages/404.html",
     "/": "/pages/index.html",
@@ -12,14 +14,19 @@ const routes = {
     "/lorem": "/pages/lorem.html",
 };
 
+// Función para manejar la ubicación actual y cargar el contenido correspondiente
 const handleLocation = async () => {
     const path = window.location.pathname;
-    const route = routes[path] || routes[404];
-    const html = await fetch(route).then((data) => data.text());
-    document.getElementById("main-page").innerHTML = html;
+    const route = routes[path] || routes[404]; // Selecciona la ruta correspondiente o la ruta 404 si no se encuentra ninguna coincidencia
+    const html = await fetch(route).then((data) => data.text()); // Carga el contenido HTML de la ruta
+    document.getElementById("main-page").innerHTML = html; // Actualiza el contenido del elemento con el id "main-page"
 };
 
+// Manejador para el evento "popstate" que se dispara cuando se realiza una navegación hacia atrás o hacia adelante en la historia del navegador
 window.onpopstate = handleLocation;
+
+// Asigna la función de enrutamiento a la ventana global para que sea accesible desde cualquier parte
 window.route = route;
 
+// Carga el contenido inicial basado en la ubicación actual
 handleLocation();
